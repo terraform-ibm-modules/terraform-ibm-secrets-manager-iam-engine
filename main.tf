c12b3166cc9b6659151d47114513596bfecc4c3e
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-03-02 21:29:49 +0000

    chore(deps): update terraform-module

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ locals {
 module "secrets_manager_group_acct" {
   count                    = (var.existing_secret_group_id == null) ? 1 : 0
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.1.3"
+  version                  = "1.1.4"
   region                   = var.region
   secrets_manager_guid     = var.secrets_manager_guid
   secret_group_name        = var.new_secret_group_name
@@ -80,7 +80,7 @@ locals {
 # Create secrets-manager secret
 module "secrets_manager_secret_iam_secret_generator_apikey" {
   source                  = "terraform-ibm-modules/secrets-manager-secret/ibm"
-  version                 = "1.1.1"
+  version                 = "1.2.0"
   region                  = var.region
   secrets_manager_guid    = var.secrets_manager_guid
   secret_name             = var.iam_secret_generator_apikey_secret_name
