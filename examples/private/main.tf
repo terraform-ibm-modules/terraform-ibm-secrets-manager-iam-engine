29ad5b17349aaff946e3f8efd34088295f3d61df
kierra.searle@ibm.com <kierra.searle@ibm.com>
2024-03-28 14:46:13 -0400

    test: testing private example

---
+++ examples/private/main.tf
@@ -0,0 +1,27 @@
+module "resource_group" {
+  source  = "terraform-ibm-modules/resource-group/ibm"
+  version = "1.1.5"
+  # if an existing resource group is not set (null) create a new one using prefix
+  resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
+  existing_resource_group_name = var.resource_group
+}
+
+# Create Secrets Manager Instance
+module "secrets_manager" {
+  source               = "terraform-ibm-modules/secrets-manager/ibm"
+  version              = "1.2.1"
+  resource_group_id    = module.resource_group.resource_group_id
+  region               = var.region
+  secrets_manager_name = "${var.prefix}-secrets-manager"
+  sm_service_plan      = "trial"
+  service_endpoints    = "private"
+  sm_tags              = var.resource_tags
+}
+
+# Configure instance with IAM engine
+module "iam_engine" {
+  source               = "../.."
+  region               = var.region
+  secrets_manager_guid = module.secrets_manager.secrets_manager_guid
+  iam_engine_name      = "generated_iam_engine"
+}
---
+++ examples/private/variables.tf
@@ -0,0 +1,33 @@
+##############################################################################
+# Input Variables
+##############################################################################
+
+variable "region" {
+  type        = string
+  description = "Region where all resources in this example will be created"
+  default     = "us-south"
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix for sm instance"
+  default     = "iam-engine-test"
+}
+
+variable "resource_group" {
+  type        = string
+  description = "An existing resource group name to use for this example, if unset a new resource group will be created"
+  default     = null
+}
+
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "An IBM Cloud apikey from an account in which resources will be created"
+  sensitive   = true
+}
+
+variable "resource_tags" {
+  type        = list(string)
+  description = "Optional list of tags to be added to created resources"
+  default     = []
+}
