f6cfb5584dffb5006f2a9050cd96b3068206dd70
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2024-04-10 16:53:50 +0100

    feature: update module and inputs for endpoint_type

--- variables.tf
+++ variables.tf
@@ -68,13 +68,13 @@ variable "display_iam_secret_generator_apikey" {
   default     = false
 }
 
-variable "service_endpoints" {
+variable "endpoint_type" {
   type        = string
   description = "The service endpoint type to communicate with the provided secrets manager instance. Possible values are `public` or `private`"
   default     = "public"
   validation {
-    condition     = contains(["public", "private"], var.service_endpoints)
-    error_message = "The specified service_endpoints is not a valid selection!"
+    condition     = contains(["public", "private"], var.endpoint_type)
+    error_message = "The specified endpoint_type is not a valid selection!"
   }
 }
 
