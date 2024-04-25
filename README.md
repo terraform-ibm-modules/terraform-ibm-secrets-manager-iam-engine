bf7ed15bd861d67f9f16818eef2f8f7aceb77664
Steve Peggs <peggs@uk.ibm.com>
2024-04-25 18:20:03 +0100

    fix: bump secrets manager 1.5.0 plus

--- README.md
+++ README.md
@@ -71,7 +71,7 @@ You need the following permissions to run this module.
 | Name | Version |
 |------|---------|
 | <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.3.0, <1.7.0 |
-| <a name="requirement_ibm"></a> [ibm](#requirement\_ibm) | >= 1.51.0 |
+| <a name="requirement_ibm"></a> [ibm](#requirement\_ibm) | >= 1.62.0 |
 
 ### Modules
 
--- examples/private/main.tf
+++ examples/private/main.tf
@@ -9,12 +9,12 @@ module "resource_group" {
 # Create Secrets Manager Instance
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "1.2.1"
+  version              = "1.10.0"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager"
   sm_service_plan      = "trial"
-  service_endpoints    = "private"
+  allowed_network      = "private-only"
   sm_tags              = var.resource_tags
 }
 
