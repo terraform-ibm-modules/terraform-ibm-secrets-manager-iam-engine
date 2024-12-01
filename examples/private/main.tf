81c4e08413efd4d9c959a5198a03060d233b70c5
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-12-01 11:09:50 -0500

    fix(deps): update terraform-module (#154)

--- examples/private/main.tf
+++ examples/private/main.tf
@@ -9,7 +9,7 @@ module "resource_group" {
 # Create Secrets Manager Instance
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "1.18.13"
+  version              = "1.19.2"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager"
